1f51417793440ef3dcfc3d0f996dc9f6e866c4be
Duan, Daniel Lee <dduan6@gatech.edu>
2022-04-22 01:04:21 -0400

    add paddles and ball

--- pong/paddle.py
+++ pong/paddle.py
@@ -1,13 +1,21 @@
 import pygame
-
-blackc = (0, 0, 0)
-
+colorb = (0,0,0)
+ 
 class Paddle(pygame.sprite.Sprite):
-
     def __init__(self, color, width, height):
         super().__init__()
         self.image = pygame.Surface([width, height])
-        self.image.fill(blackc)
-        self.image.set_colorkey(blackc)
+        self.image.fill(colorb)
+        self.image.set_colorkey(colorb)
         pygame.draw.rect(self.image, color, [0, 0, width, height])
-        self.rect = self.image.get_rect
+        self.rect = self.image.get_rect()
+        
+    def moveUp(self, pixels):
+        self.rect.y -= pixels
+        if self.rect.y < 0:
+          self.rect.y = 0
+          
+    def moveDown(self, pixels):
+        self.rect.y += pixels
+        if self.rect.y > 400:
+          self.rect.y = 400
--- pong/pong.py
+++ pong/pong.py
@@ -1,44 +1,93 @@
-import black
-import pygame;
-pygame.init()
-
+import pygame
 from paddle import Paddle
-
-blackc = (0, 0, 0)
-whitec = (0, 0, 0)
-
+from ball import Ball
+ 
+pygame.init()
+ 
+colorb = (0,0,0)
+colorw = (255,255,255)
+ 
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pong")
-
-paddleA = Paddle(whitec, 10, 100)
+ 
+paddleA = Paddle(colorw, 10, 100)
 paddleA.rect.x = 20
 paddleA.rect.y = 200
-paddleB = Paddle(whitec, 10, 100)
+ 
+paddleB = Paddle(colorw, 10, 100)
 paddleB.rect.x = 670
 paddleB.rect.y = 200
 
+ball = Ball(colorw, 10, 10)
+ball.rect.x = 345
+ball.rect.y = 195
+ 
 sprites_list = pygame.sprite.Group()
+ 
 sprites_list.add(paddleA)
 sprites_list.add(paddleB)
+sprites_list.add(ball)
+ 
+carryOn = True
+ 
+clock = pygame.time.Clock()
+ 
+aScore = 0
+bScore = 0
 
-time = pygame.time.Clock()
-playing = true
-
-while playing:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            playing = false
-
+while carryOn:
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+              carryOn = False 
+        elif event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_x: 
+                     carryOn=False
+ 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        paddleA.moveUp(5)
+    if keys[pygame.K_s]:
+        paddleA.moveDown(5)
+    if keys[pygame.K_UP]:
+        paddleB.moveUp(5)
+    if keys[pygame.K_DOWN]:
+        paddleB.moveDown(5)    
+ 
     sprites_list.update()
+ 
+    if ball.rect.x >= 690:
+        aScore += 1
+        ball.velocity[0] = -ball.velocity[0]
+        
+    if ball.rect.x <= 0:
+        bScore += 1
+        ball.velocity[0] = -ball.velocity[0]
 
-    screen.fill(blackc)
-    pygame.draw.line(screen, WHITE, [349, 0], [349, 500], 5)
+    if ball.rect.x >= 690:
+        ball.velocity[0] = -ball.velocity[0]
+    if ball.rect.x <= 0:
+        ball.velocity[0] = -ball.velocity[0]
+    if ball.rect.y > 490:
+        ball.velocity[1] = -ball.velocity[1]
+    if ball.rect.y < 0:
+        ball.velocity[1] = -ball.velocity[1] 
+
+    if pygame.sprite.collide_mask(ball, paddleA) or pygame.sprite.collide_mask(ball, paddleB):
+      ball.bounce()
+
+    screen.fill(colorb)
+    pygame.draw.line(screen, colorw, [349, 0], [349, 500], 5)
     sprites_list.draw(screen)
-    
+
+    font = pygame.font.Font(None, 74)
+    text = font.render(str(aScore), 1, colorw)
+    screen.blit(text, (250,10))
+    text = font.render(str(bScore), 1, colorw)
+    screen.blit(text, (420,10))
+
     pygame.display.flip()
-    time.tick(60)
-
-
-
-pygame.quit
+     
+    clock.tick(60)
+ 
+pygame.quit()
